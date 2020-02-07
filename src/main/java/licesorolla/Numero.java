025bc2e61573945294de2e564264aedd7d7b5c33
alexs <alexs@LAPTOP-KQGHEVBO>
2020-02-07 20:19:19 +0100

    mejora6

--- src/main/java/licesorolla/Numero.java
+++ src/main/java/licesorolla/Numero.java
@@ -14,10 +14,20 @@ public class Numero {
 		    else
 		       System.out.println("el numero no es primo");
 	}
+	
+	public static void calcularFactorial(int numeroP) {
+		int resultado=1;
+		 for(int i = 1;i<=numeroP;i++) {
+			 resultado = resultado * i;
+		 }
+		 
+		 System.out.print("El factorial es "+resultado);
+	}
 	public static void main(String[] args) {
 		
 		int numeroP = 5;
 		calcularPrimo(numeroP);
+		calcularFactorial(numeroP);
 	}
 
 }
