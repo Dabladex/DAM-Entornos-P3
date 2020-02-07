44ca8dd1b92fd1eaa054a48fe7b1733472616b67
alexs <alexs@LAPTOP-KQGHEVBO>
2020-02-07 20:10:17 +0100

    mejora5

--- src/main/java/licesorolla/Numero.java
+++ src/main/java/licesorolla/Numero.java
@@ -1,11 +1,23 @@
 package main.java.licesorolla;
 
 public class Numero {
-
+	public static void calcularPrimo(int numeroP) {
+		 boolean prime = true; 
+		    for(int i = 2; i < numeroP; i++) {
+		        if (numeroP % i == 0) {
+		            prime = false;
+		            break;
+		        }
+		    }
+		    if (prime)
+		       System.out.println("El numero es primo");
+		    else
+		       System.out.println("el numero no es primo");
+	}
 	public static void main(String[] args) {
 		
 		int numeroP = 5;
-
+		calcularPrimo(numeroP);
 	}
 
 }
